182747a6e7b7b012104a9df4765fc4474017a750
Yvonne Danisch <72450125+Kazouha@users.noreply.github.com>
2021-11-20 18:30:33 +0100

    Create INI3_Strings_and_Lists.py

---
+++ Python_Village/INI3_Strings_and_Lists.py
@@ -0,0 +1,10 @@
+# Input variables
+input_string = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+
+a = 22 
+b = 27 
+c = 97 
+d = 102
+
+# Print result
+print(input_string[a:b+1]+ " " + input_string[c:d+1])
