7ab47a16e5ee8f507675550299d3c8145a6b2054
Yvonne Danisch <72450125+Kazouha@users.noreply.github.com>
2021-11-22 09:34:55 +0100

    Add files via upload

---
+++ Python_Village/StringsAndLists.java
@@ -0,0 +1,15 @@
+public class StringsAndLists {
+    public static void main(String[] args) {
+        // Input variables
+        String inputString = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.";
+
+        int a = 22; 
+        int b = 27; 
+        int c = 97; 
+        int d = 102;
+
+        // Print result
+        System.out.println(inputString.substring(a, b+1) + " " + inputString.subSequence(c, d+1));
+    }
+    
+}
