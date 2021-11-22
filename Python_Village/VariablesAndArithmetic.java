c9562907bfe62c6d7616982432290251e4252aaf
Yvonne Danisch <72450125+Kazouha@users.noreply.github.com>
2021-11-22 09:26:01 +0100

    Add files via upload

---
+++ Python_Village/VariablesAndArithmetic.java
@@ -0,0 +1,12 @@
+public class VariablesAndArithmetic {
+    public static void main(String[] args) {
+        // Input variables
+        int a = 3;
+        int b = 5;
+
+        // Calculate result
+        int cSquared = a*a + b*b;
+
+        System.out.println(cSquared);
+    }
+}
