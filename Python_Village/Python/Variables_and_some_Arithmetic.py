9079418e46e625e5b62b7dd56929adffbc81e9d4
Yvonne Danisch <yvonnedanisch@gmx.de>
2023-05-05 14:54:40 +0200

    FIX: Small changes in comments

--- Python_Village/Python/Variables_and_some_Arithmetic.py
+++ Python_Village/Python/Variables_and_some_Arithmetic.py
@@ -1,6 +1,6 @@
 # Given: Two positive integers a and b, each less than 1000.
-#Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs
-#have lengths a and b.
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs
+# have lengths a and b.
 
 # Reads file content into list
 def read_file(file_name):
