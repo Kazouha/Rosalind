63519e546881766bbe6e5c11440637db40b36909
Yvonne Danisch <yvonnedanisch@gmx.de>
2023-05-05 13:33:08 +0200

    ADD: Installing_Python.py

---
+++ Python_Village/Python/Installing_Python.py
@@ -0,0 +1,4 @@
+# After downloading and installing Python, type "import this" into the Python command line and see 
+# what happens.
+
+import this       # Running this will print the poem "The Zen of Python" by Tim Peters
